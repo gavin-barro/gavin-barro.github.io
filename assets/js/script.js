6cf185f0386d03a7bd4ce79d9afc0c174825406e
Gavin Barro <g.ab28320@gmail.com>
2025-05-30 18:33:53 -0400

    basic dark mode stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
+// Back to top button
 document.addEventListener("DOMContentLoaded", function () {
-  const backToTopBtn = document.getElementById("backToTop");
+  let backToTopBtn = document.getElementById("backToTop");
   if (backToTopBtn) {
     backToTopBtn.addEventListener("click", function () {
       window.scrollTo({ top: 0, behavior: "smooth" });
     });
   }
+});
+
+// Dark mode
+let toggleBtn = document.getElementById('darkModeToggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
 });
